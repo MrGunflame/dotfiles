acaca6bd9d35772e43cd85cf527de8ef55509a4b
MrGunflame <mrgunflame@protonmail.com>
2023-03-13 18:34:19 +0100

    Lsp inline errors

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,8 +1,24 @@
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
-lsp.setup()
+lsp.preset({
+    name = "minimal",
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+
+    suggest_lsp_servers = false,
+})
 
 lsp.ensure_installed({
 	"rust_analyzer",
 })
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = false,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+})
+
